9e0212cd32ad0cc874887ed16da0a3c0c05f3a61
Stralax <strahinjadjordjevic2003@gmail.com>
2025-04-12 23:46:28 +0200

    working

--- minimax-app/src/app/app.routes.ts
+++ minimax-app/src/app/app.routes.ts
@@ -3,10 +3,6 @@ import { CoffeeComponent } from './components/coffeeStart/coffee/coffee.componen
 import { CoffePlaneviewComponent } from './components/coffe-planeview/coffe-planeview.component';
 
 export const routes: Routes = [
-<<<<<<< HEAD
     { path: '', component: CoffeeComponent },
     { path: 'coffee', component: CoffePlaneviewComponent }
-=======
-    { path: '', component: CoffeeComponent }
->>>>>>> 33c5b80d57155245346056e76c1cace3969ba4b8
 ];
