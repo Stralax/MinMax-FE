03b3796e117ec738f1a0bbcb32242b91c7aafa1c
sonja184 <sonjagavranovic18@gmail.com>
2025-04-12 16:43:55 +0200

    Fix

---
+++ minimax-app/src/app/components/coffeeStart/coffee/coffee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee',
+  imports: [],
+  templateUrl: "./coffee.component.html",
+  styles: ``
+})
+export class CoffeeComponent {
+
+}
