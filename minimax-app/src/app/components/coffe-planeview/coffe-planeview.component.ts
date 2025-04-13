b2003126a7055f24e64ab5cda76904001a93be57
sonja184 <sonjagavranovic18@gmail.com>
2025-04-13 07:26:45 +0200

    route

--- minimax-app/src/app/components/coffe-planeview/coffe-planeview.component.ts
+++ minimax-app/src/app/components/coffe-planeview/coffe-planeview.component.ts
@@ -23,8 +23,10 @@ export class CoffePlaneviewComponent {
     private router: Router
   ) {} // Fixed constructor syntax
 
+
   async onSubmit(): Promise<void> {
     console.log("ALO");
+    
     for(let i=0; i<1; i++){
       let p = await this.linkService.getLink(this.uploadedImages[i]);
       this.link = p.toString();
@@ -33,6 +35,7 @@ export class CoffePlaneviewComponent {
       // console.log(this.product.images);
       console.log(this.link);
     }
+    this.router.navigate(['chatbot']);
   }
 
   // Suggested additional method for image handling
