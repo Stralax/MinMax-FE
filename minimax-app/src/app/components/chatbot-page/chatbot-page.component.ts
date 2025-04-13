578cb9900caa33ddc60f992941227e40b396a169
sonja184 <sonjagavranovic18@gmail.com>
2025-04-13 05:47:04 +0200

    latest

---
+++ minimax-app/src/app/components/chatbot-page/chatbot-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatbot-page',
+  imports: [],
+  templateUrl: './chatbot-page.component.html',
+  styleUrl: './chatbot-page.component.css'
+})
+export class ChatbotPageComponent {
+
+}
